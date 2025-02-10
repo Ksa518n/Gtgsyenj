13df36d0a2734cfda5d8ac5840286f862e84e202
Ksa518n <159606491+Ksa518n@users.noreply.github.com>
2025-02-10 20:26:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+let cart = [];
 
+function addToCart(productName, price) {
+    cart.push({ name: productName, price: price });
+    alert(`${productName} تمت إضافته إلى السلة!`);
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
