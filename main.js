6baab74c4c2dd9067f2774ad92753618ab99f877
Ksa518n <159606491+Ksa518n@users.noreply.github.com>
2025-02-11 11:43:21 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const productContainer = document.getElementById('products');
+    const productList = document.getElementById('product-list');
     const products = JSON.parse(localStorage.getItem('products')) || [];
 
+    productList.innerHTML = '';
     products.forEach(product => {
         const productDiv = document.createElement('div');
         productDiv.classList.add('product-item');
-
         productDiv.innerHTML = `
             <img src="${product.image}" alt="${product.name}">
             <h3>${product.name}</h3>
             <p>الوزن: ${product.weight} كجم</p>
-            <p>العمر: ${product.age} شهور</p>
+            <p>العمر: ${product.age}</p>
             <p>السعر: ${product.price} ريال</p>
         `;
-
-        productContainer.appendChild(productDiv);
+        productList.appendChild(productDiv);
     });
 });
